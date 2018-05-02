26d59a0edb0c22aac2c985fa6acb3c929bbc3b78
Mathias Witt <mathias.witt@axelspringer.de>
2018-05-02 15:45:11 +0200

    renaming namespace, filename and update composer file

---
+++ wp-mu-action-post.php
@@ -0,0 +1,23 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+Class Axelspringer_Post {
+
+    public function __construct() {
+        add_action( 'save_post', array( $this, 'set_post_guid' ) );
+        add_action( 'edit_attachment', array( $this, 'set_post_guid' ) );
+        add_action( 'add_attachment', array( $this, 'set_post_guid' ) );
+    }
+
+    public function set_post_guid( $post_id ) {
+        global $wpdb;
+        $where = array( 'ID' => $post_id );
+        $wpdb->update( $wpdb->posts, array( 'guid' => $post_id) , $where );
+
+        return $post_id;
+    }
+
+}
+
+$axelspringer_post = new Axelspringer_Post();
